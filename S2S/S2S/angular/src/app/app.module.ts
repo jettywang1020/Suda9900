b1aea6d62718bc04cf9716015ab438d605ab35a5
MrTuesday1020 <mrtuesday1020@gmail.com>
2018-08-09 16:53:50 +1000

    .

--- S2S/S2S/angular/src/app/app.module.ts
+++ S2S/S2S/angular/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
+import { TestComponent } from './test/test.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
