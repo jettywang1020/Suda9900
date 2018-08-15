0fba508b4f928f33654e31f2e029afcfda9294f0
王丰羽 <wangfengyu@wangfengyudeMacBook-Pro.local>
2018-08-15 15:53:18 +1000

    Diray

--- S2S/S2S/public/admin.py
+++ S2S/S2S/public/admin.py
@@ -1,15 +1,16 @@
 from django.contrib import admin
 from .models import *
 
-# class TenantAdmin(admin.ModelAdmin):
-# 	list_display = ['id','username','email']
-#
-# class LandLordAdmin(admin.ModelAdmin):
-# 	list_display = ['id','username','email']
-#
-# class HouseAdmin(admin.ModelAdmin):
-# 	list_display = ['id','housename','postcode']
-#
-# admin.site.register(Tenant,TenantAdmin)
-# admin.site.register(Landlord,LandLordAdmin)
-# admin.site.register(House,HouseAdmin)
+class TenantAdmin(admin.ModelAdmin):
+	list_display = ['id','username']
+
+class LandLordAdmin(admin.ModelAdmin):
+	list_display = ['id','username']
+
+class HouseAdmin(admin.ModelAdmin):
+	list_display = ['id','name']
+
+
+admin.site.register(Tenant,TenantAdmin)
+admin.site.register(Landlord,LandLordAdmin)
+admin.site.register(House,HouseAdmin)
--- S2S/S2S/public/models.py
+++ S2S/S2S/public/models.py
@@ -2,17 +2,19 @@ from django.db import models
 
 # Create your models here.
 class Tenant(models.Model):
+	gender_choices = (('M','Male'),('F','Female'),)
 	time_stamp = models.DateTimeField(auto_now_add=True)
 	username = models.CharField(max_length=64, null=False)
 	password = models.CharField(max_length=256, null=False)
 	phone = models.CharField(max_length=128, null=False)
 	email = models.EmailField(null=False)
-	photo = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
-	gender = models.BooleanField(default=False)
-	dob = models.DateField()
+	#photo = models.ImageField(upload_to='tenant_photo', default='tenant_photo/default.jpg', blank=True)
+	gender = models.CharField(max_length=1, choices=gender_choices, default='M')
+	dob = models.DateField(null=True,auto_now=False, auto_now_add=False)
 	profile = models.TextField(null=True, blank=False)
 	activate = models.BooleanField(default=False)
 	status = models.BooleanField(default=True)
+
 
 	class Meta:
 		db_table = "tenant"
@@ -21,14 +23,15 @@ class Tenant(models.Model):
 		return self.username
 
 class Landlord(models.Model):
+	gender_choices = (('M', 'Male'), ('F', 'Female'),)
 	time_stamp = models.DateTimeField(auto_now_add=True)
 	username = models.CharField(max_length=64, null=False)
 	password = models.CharField(max_length=256, null=False)
 	phone = models.CharField(max_length=128, null=False)
 	email = models.EmailField(null=False)
-	photo = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
-	gender = models.BooleanField(default=False)
-	dob = models.DateField()
+	#photo = models.ImageField(upload_to='landlord_photo', default='landlord_photo/default.jpg', blank=True)
+	gender = models.CharField(max_length=1, choices=gender_choices, default='M')
+	dob = models.DateField(null=True, auto_now=False, auto_now_add=False)
 	profile = models.TextField(null=True, blank=False)
 	activate = models.BooleanField(default=False)
 	status = models.BooleanField(default=True)
@@ -69,8 +72,69 @@ class House(models.Model):
 		db_table = "house"
 
 	def __str__(self):
-		return self.housename
+		return self.name
 
+class House_Picture(models.Model):
+	time_stamp = models.DateTimeField(auto_now_add=True)
+	house_id = models.IntegerField(null=False)
+	photo = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
+
+	class Meta:
+		db_table = "house_picture"
+
+class Lease_Period(models.Model):
+	time_stamp = models.DateTimeField(auto_now_add=True)
+	house_id = models.IntegerField(null=False)
+	period_start = models.DateField(null=False)
+	period_end = models.DateField(null=False)
+
+	class Meta:
+		db_table = "lease_period"
+
+class Tag(models.Model):
+	time_stamp = models.DateTimeField(auto_now_add=True)
+	tag = models.CharField(max_length=64, null=False)
+
+	class Meta:
+		db_table = "tag"
+
+class Tenant_Tag(models.Model):
+	time_stamp = models.DateTimeField(auto_now_add=True)
+	tenant_id = models.IntegerField(null=False)
+	tag_id = models.IntegerField(null=False)
+
+	class Meta:
+		db_table = "tenant_tag"
+
+class House_Tag(models.Model):
+	time_stamp = models.DateTimeField(auto_now_add=True)
+	house_id = models.IntegerField(null=False)
+	tag_id = models.IntegerField(null=False)
+
+	class Meta:
+		db_table = "house_tag"
+
+class Tenant_Rate(models.Model):
+	time_stamp = models.DateTimeField(auto_now_add=True)
+	tenant_id = models.IntegerField(null=False)
+	landlord_id = models.IntegerField(null=False)
+	reputation = models.IntegerField(null=False)
+
+	class Meta:
+		db_table = "tenant_rate"
+
+class House_Rate(models.Model):
+	time_stamp = models.DateTimeField(auto_now_add=True)
+	tenant_id = models.IntegerField(null=False)
+	accuracy = models.IntegerField(null=False)
+	communication = models.IntegerField(null=False)
+	cleanliness = models.IntegerField(null=False)
+	location = models.IntegerField(null=False)
+	check_in = models.IntegerField(null=False)
+	value = models.IntegerField(null=False)
+
+	class Meta:
+		db_table = "house_rate"
 
 class House_Comment(models.Model):
 	time_stamp = models.DateTimeField(auto_now_add=True)
