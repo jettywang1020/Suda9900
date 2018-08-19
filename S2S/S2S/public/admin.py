3cdddfce948490320d0194358b1a11b665ffbdb1
王丰羽 <wangfengyu@wangfengyudeMacBook-Pro.local>
2018-08-19 23:30:46 +1000

    Diary

--- S2S/S2S/public/admin.py
+++ S2S/S2S/public/admin.py
@@ -3,12 +3,27 @@ from .models import *
 
 class TenantAdmin(admin.ModelAdmin):
 	list_display = ['id','username']
+	list_filter = ['username']
+	search_fields = ['username']
+	list_per_page = 10
+	fields = ['username','password','phone','email','gender','dob','profile','activate','status']
 
 class LandLordAdmin(admin.ModelAdmin):
 	list_display = ['id','username']
+	list_filter = ['username']
+	search_fields = ['username']
+	list_per_page = 10
+	fields = ['username', 'password', 'phone', 'email', 'gender', 'dob', 'profile', 'activate', 'status']
 
 class HouseAdmin(admin.ModelAdmin):
 	list_display = ['id','name']
+	list_filter = ['name']
+	search_fields = ['name']
+	list_per_page = 10
+	fieldsets = [
+		('Basic Information', {'fields': ['landlord_id','name','address','postcode','price','profile']}),
+		('House detail', {'fields': ['bpub_date', 'bpub1_date']}),
+	]
 
 
 admin.site.register(Tenant,TenantAdmin)
