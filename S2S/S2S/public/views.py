b55908bceb839be801131fa20714d0a8d020bd80
atobeclaq <31129897+atobeclaq@users.noreply.github.com>
2018-08-27 17:12:44 +1000

    Diary Working Note

--- S2S/S2S/public/views.py
+++ S2S/S2S/public/views.py
@@ -9,31 +9,31 @@ def index(request):
 	hello = 'hello, everyone'
 	return render(request, 'public/index.html', {'hello': hello})
 
-<<<<<<< HEAD
+
 
 def search(request):
 	return render(request, 'public/search.html')
-=======
+
 ##### signup page #####
 def signup(request):
 	return render(request, 'public/signup.html')
->>>>>>> 6994ddb475429037f9de9b154874a42100c528a4
+
 
 ##### signup page raw #####
 def Register(request):
 	return render(request, 'public/Register.html')
 
-<<<<<<< HEAD
+
 def view_detail(request):
 	return render(request, 'public/View_Detail.html')
 
 
-=======
+
 
 ##### search house page #####
 def Home(request):
 	return render(request, 'public/Home.html')
->>>>>>> 6994ddb475429037f9de9b154874a42100c528a4
+
 
 ##### row houses page #####
 def Display(request):
