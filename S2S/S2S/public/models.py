4a5fc95a4fa1ebc89ff44bfec89e4c9111e4b2ba
MrTuesday1020 <mrtuesday1020@gmail.com>
2018-08-27 12:42:52 +1000

    models

--- S2S/S2S/public/models.py
+++ S2S/S2S/public/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Tenant(models.Model):
+class Usesr(models.Model):
 	gender_choices = (('M','Male'),('F','Female'),)
 	time_stamp = models.DateTimeField(auto_now_add=True)
 	username = models.CharField(max_length=64, null=False)
@@ -10,45 +10,24 @@ class Tenant(models.Model):
 	password = models.CharField(max_length=256, null=False)
 	email = models.EmailField(null=False)
 	phone = models.CharField(max_length=128, null=True)
-	#photo = models.ImageField(upload_to='tenant_photo', default='tenant_photo/default.jpg', blank=True)
+	#photo = models.ImageField(upload_to='user', default='user/default.jpg', blank=True)
 	gender = models.CharField(max_length=1, choices=gender_choices, default='M')
 	dob = models.DateField(null=True,auto_now=False, auto_now_add=False)
-	profile = models.TextField(null=True, blank=False)
+	profile = models.TextField(null=True, blank=True)
 	activate = models.BooleanField(default=False)
 	status = models.BooleanField(default=True)
-
+	is_landlord = models.BooleanField(default=False)
 
 	class Meta:
-		db_table = "tenant"
+		db_table = "user"
 
 	def __str__(self):
 		return self.username
 
-class Landlord(models.Model):
-	gender_choices = (('M', 'Male'), ('F', 'Female'),)
-	time_stamp = models.DateTimeField(auto_now_add=True)
-	username = models.CharField(max_length=64, null=False)
-	first_name = models.CharField(max_length=64, null=False)
-	last_name = models.CharField(max_length=64, null=False)
-	password = models.CharField(max_length=256, null=False)
-	email = models.EmailField(null=False)
-	phone = models.CharField(max_length=128, null=True)
-	#photo = models.ImageField(upload_to='landlord_photo', default='landlord_photo/default.jpg', blank=True)
-	gender = models.CharField(max_length=1, choices=gender_choices, default='M')
-	dob = models.DateField(null=True, auto_now=False, auto_now_add=False)
-	profile = models.TextField(null=True, blank=False)
-	activate = models.BooleanField(default=False)
-	status = models.BooleanField(default=True)
-
-	class Meta:
-		db_table = "landlord"
-
-	def __str__(self):
-		return self.username
 
 class House(models.Model):
 	time_stamp = models.DateTimeField(auto_now_add=True)
-	landord_id = models.IntegerField(null=False)
+	user_id = models.IntegerField(null=False)
 	name = models.CharField(max_length=256, null=False)
 	address = models.CharField(max_length=256, null=False)
 	postcode = models.IntegerField(null=False)
@@ -69,7 +48,7 @@ class House(models.Model):
 	pool = models.BooleanField(default=False)
 	house_rule = models.TextField(null=False, blank=False)
 	cancellation = models.TextField(null=False, blank=False)
-	extra = models.TextField(null=True, blank=False)
+	extra = models.TextField(null=True, blank=True)
 	status = models.BooleanField(default=True)
 
 	class Meta:
@@ -81,7 +60,7 @@ class House(models.Model):
 class House_Picture(models.Model):
 	time_stamp = models.DateTimeField(auto_now_add=True)
 	house_id = models.IntegerField(null=False)
-	photo = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
+	#photo = models.ImageField(upload_to='house', default='house/default.jpg', blank=True)
 
 	def __str__(self):
 		return self.house_id
@@ -111,9 +90,9 @@ class Tag(models.Model):
 	class Meta:
 		db_table = "tag"
 
-class Tenant_Tag(models.Model):
+class User_Tag(models.Model):
 	time_stamp = models.DateTimeField(auto_now_add=True)
-	tenant_id = models.IntegerField(null=False)
+	user_id = models.IntegerField(null=False)
 	tag_id = models.IntegerField(null=False)
 
 	def __str__(self):
@@ -133,10 +112,11 @@ class House_Tag(models.Model):
 	class Meta:
 		db_table = "house_tag"
 
-class Tenant_Rate(models.Model):
+class User_Rate(models.Model):
 	time_stamp = models.DateTimeField(auto_now_add=True)
-	tenant_id = models.IntegerField(null=False)
-	landlord_id = models.IntegerField(null=False)
+	# user1 is tenant, user2 is landlord
+	user1_id = models.IntegerField(null=False)
+	user2_id = models.IntegerField(null=False)
 	reputation = models.IntegerField(null=False)
 
 	def __str__(self):
@@ -147,6 +127,7 @@ class Tenant_Rate(models.Model):
 
 class House_Rate(models.Model):
 	time_stamp = models.DateTimeField(auto_now_add=True)
+	user_id = models.IntegerField(null=False)
 	house_id = models.IntegerField(null=False)
 	accuracy = models.IntegerField(null=False)
 	communication = models.IntegerField(null=False)
@@ -163,6 +144,7 @@ class House_Rate(models.Model):
 
 class House_Comment(models.Model):
 	time_stamp = models.DateTimeField(auto_now_add=True)
+	user_id = models.IntegerField(null=False)
 	house_id = models.IntegerField(null=False)
 	comment = models.TextField(null=False, blank=False)
 
