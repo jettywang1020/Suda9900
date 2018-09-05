ab30c6671368df90445af9eec14f96abde536d54
王丰羽 <wangfengyu@wangfengyudeMacBook-Pro.local>
2018-09-05 14:17:27 +1000

    Diary

--- S2S/S2S/landlord/views.py
+++ S2S/S2S/landlord/views.py
@@ -11,6 +11,7 @@ def index(request):
 
 ##### Landlord add house #####
 def add_house(request):
+	id = 1
 	originalform = addhouse_form()
 	if request.method == 'POST':
 		form = addhouse_form(request.POST)
@@ -18,6 +19,35 @@ def add_house(request):
 			name = form.cleaned_data.get("name")
 			address = form.cleaned_data.get("address")
 			postcode = form.cleaned_data.get("postcode")
+			price = form.cleaned_data.get("price")
+			profile = form.cleaned_data.get("profile")
+			maxguest = form.cleaned_data.get("maxguest")
+			bed = form.cleaned_data.get("bed")
+			bedroom = form.cleaned_data.get("bedroom")
+			bathroom = form.cleaned_data.get("bathroom")
+			park = form.cleaned_data.get("park")
+			tv = form.cleaned_data.get("tv")
+			kitchen = form.cleaned_data.get("kitchen")
+			washer = form.cleaned_data.get("washer")
+			fridge = form.cleaned_data.get("fridge")
+			conditioner = form.cleaned_data.get("conditioner")
+			wifi = form.cleaned_data.get("wifi")
+			studyroom = form.cleaned_data.get("studyroom")
+			pool = form.cleaned_data.get("pool")
+			rule = form.cleaned_data.get("rule")
+			cancellation = form.cleaned_data.get("cancellation")
+			extra = form.cleaned_data.get("extra")
+			house = House(user_id = id, name = name, address = address, postcode = postcode, price = price, profile = profile
+						  , max_guests = maxguest, no_of_beds = bed, no_of_bedrooms = bedroom, no_of_baths = bathroom
+						  , no_of_parking = park, tv = tv, kitchen = kitchen, washer = washer, fridge = fridge
+						  , conditioner = conditioner, wifi = wifi, study_room = studyroom, pool = pool, house_rule = rule
+						  , cancellation = cancellation, extra = extra)
+			house.save()
+			request.session['house_account'] = {'id':house.id, 'user_id':id, 'name':name,'address':address, 'postcode':postcode, 'price':price
+												, 'profile':profile, 'max_guests':maxguest, 'no_of_beds':bed, 'no_of_bedrooms':bedroom
+												, 'no_of_baths':bathroom, 'no_of_parking':park, 'tv':tv, 'kitchen':kitchen, 'washer':washer
+												, 'fridge':fridge, 'conditioner':conditioner, 'wifi':wifi, 'study_room':studyroom, 'pool':pool
+												, 'house_rule':rule, 'cancellation':cancellation, 'extra':extra}
 	return render(request, 'landlord/add_house.html',{'form': originalform})
 
 def manage_house(request):
