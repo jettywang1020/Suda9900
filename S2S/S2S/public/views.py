6c57a599759fc5edcf03fcc1bb6d0e9a6c88b073
MrTuesday1020 <mrtuesday1020@gmail.com>
2018-10-16 14:39:07 +1100

    .

--- S2S/S2S/public/views.py
+++ S2S/S2S/public/views.py
@@ -286,7 +286,10 @@ def display(request):
 		return render(request, 'public/display.html', {'houses':houses,'r_houses':relate})
 
 def profile(request):
-	id = request.session['account']['id'] if 'account' in request.session else 0
+	try:
+		id = request.session['account']['id'] if 'account' in request.session else 0
+	except:
+		return redirect('public:login')
 	user = User.objects.get(pk=id)
 	originalform = profile_form()
 	if request.method == 'POST':
@@ -325,7 +328,10 @@ def profile(request):
 		return render(request, 'public/profile.html', {'form': originalform})
 
 def upload_photo(request):
-	id = request.session['account']['id'] if 'account' in request.session else 0
+	try:
+		id = request.session['account']['id'] if 'account' in request.session else 0
+	except:
+		return redirect('public:login')
 	user = User.objects.get(pk=id)
 	originalform = upload_photo_form()
 	if request.method == 'POST':
@@ -338,7 +344,10 @@ def upload_photo(request):
 
 
 def book(request):
-	user_id = request.session['account']['id'] if 'account' in request.session else 0
+	try:
+		user_id = request.session['account']['id'] if 'account' in request.session else 0
+	except:
+		return redirect('public:login')
 	if request.method == 'POST':
 		house_id = request.POST.get("house_id",None)
 		check_in = request.POST.get("check_in",None)
